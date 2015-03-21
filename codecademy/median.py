cfd344db3045345180d4f1810e823ed9c015faf3
jcnt <yyaazz@gmail.com>
2015-03-21 07:53:45 +0100

    fix median.py

--- codecademy/median.py
+++ codecademy/median.py
@@ -1,12 +1,15 @@
 def median(inlist):
     slist = sorted(inlist)
     print slist
+    if len(inlist) == 1:
+        return inlist[0]
     if len(inlist) % 2 == 0:
         emid = len(slist) / 2
         return (float(slist[emid-1]) + float(slist[emid]))/2
     if len(inlist) % 2 == 1:
-        return len(slist) / 2
+        print slist
+        return slist[(len(slist) / 2)]
         
 
-median([1,2,3,4,5])
+median([6, 8, 12, 2])
 
