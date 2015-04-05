1ddcf2bfe3e965be29578d3c8b2067362482e472
jcnt <yyaazz@gmail.com>
2015-04-05 09:20:59 +0200

    adding Ruby

---
+++ ruby/redact.rb
@@ -0,0 +1,20 @@
+puts "type in some text"
+text = gets.chomp
+puts "words to redact"
+redact = gets.chomp
+
+words = text.split(" ")
+red = redact.split(" ")
+
+words.each do |word|
+    red.each do |reda|
+        if word == reda
+            print "REDACTED "
+            next
+            next
+        end
+        print word + " "
+    end
+end
+
+
